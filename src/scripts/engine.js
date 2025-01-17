782f74df4285f63d55d10f8bdd98791558e5ace9
Jonas Vitorio <jonasvitorio35@gmail.com>
2025-01-17 11:39:13 -0300

    I addited a favicon

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -199,6 +199,8 @@ async function playAudio(status) {
 function init() {
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
+
+    ShowHiddenCardFieldsImages(false);
 }
 
 init();
