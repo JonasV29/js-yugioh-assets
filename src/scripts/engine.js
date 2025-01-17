cd34adcc1c97443a68f4990f3c5433e86f9cab27
Jonas Vitorio <jonasvitorio35@gmail.com>
2025-01-17 11:29:17 -0300

    I put extract to method in javascript

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -83,16 +83,37 @@ async function setcardsField(cardId) {
 
     let computerCardId = await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block";
-    state.fieldCards.computer.style.display = "block";
+    await hiddenCardDetails();
 
-    state.fieldCards.player.src = cardData[cardId].img;
-    state.fieldCards.computer.src = cardData[computerCardId].img;
+    await ShowHiddenCardFieldsImages(true);
+
+    await drawCardsInField(cardId,computerCardId);
 
      let duelResults = await checkDuelResults(cardId, computerCardId);
 
      await updateScore();
      await drawButton(duelResults); 
+}
+
+async function drawCardsInField(cardId,computerCardId){
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+}
+
+async function ShowHiddenCardFieldsImages(value) {
+    if(value === true) {
+        state.fieldCards.player.style.display = "block";
+        state.fieldCards.computer.style.display = "block";
+    } else if(value === false) {
+        state.fieldCards.player.style.display = "none";
+        state.fieldCards.computer.style.display = "none";
+    }
+}
+
+async function hiddenCardDetails() {
+    state.cardSprites.avatar.src = "";
+    state.cardSprites.name.innerText = "";
+    state.cardSprites.type.innerText = "";
 }
 
 async function updateScore() {
